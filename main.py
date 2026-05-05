b4e737b9f2cc203879294c996ec96e5ed06dbcfb
Ivan-alt2 <2011bor6@gmail.com>
2026-05-05 19:00:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
-from typing import List
+def decode_string(s: str) -> str:
 
-# ID успешной попытки: 160965005
+# ID успешной попытки: 161571272	
 
-def calculate_min_platforms(weights: List[int], limit: int) -> int:
-    sorted_weights = sorted(weights)
-    platforms = 0
-    left = 0
-    right = len(sorted_weights) - 1
+    def decode_recursive(index: int) -> tuple[str, int]:
+        result = []
+        current_num = 0
 
-    while left <= right:
-        if sorted_weights[left] + sorted_weights[right] <= limit:
-            left += 1
-        right -= 1
-        platforms += 1
+        while index < len(s):
+            char = s[index]
 
-    return platforms
+            if char.isdigit():
+                current_num = current_num * 10 + int(char)
+                index += 1
 
+            elif char == '[':
+                substring, next_index = decode_recursive(index + 1)
+                result.append(substring * current_num)
+                current_num = 0
+                index = next_index
 
-def main() -> None:
-    weights_input = list(map(int, input().split()))
-    limit_input = int(input())
+            elif char == ']':
+                return ''.join(result), index + 1
 
-    result = calculate_min_platforms(weights_input, limit_input)
+            else:
+                result.append(char)
+                index += 1
 
-    print(result)
+        return ''.join(result), index
+
+    decoded_string, _ = decode_recursive(0)
+    return decoded_string
 
 
 if __name__ == "__main__":
-    main()
+    compressed_input = input().strip()
+    print(decode_string(compressed_input))
